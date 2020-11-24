0c2be444f607feaf93e3f83493d48527a9e49bc2
Akash <74901322+iakash0511@users.noreply.github.com>
2020-11-23 23:10:11 -0800

    Add files via upload

---
+++ day 2.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+a=b=c = 5
+print (a/10)
+print (b*50)
+print (c+60)
+string = "Akash"
+print (string)
+print (string.replace('a','g'))
+a = (5)
+b = (0.5)
+print (a)
+print (b)
+a = float(a)
+b = int(b)
+print (a)
+print (b)
